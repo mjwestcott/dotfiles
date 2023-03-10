7117b631f78bb310dbbbee9178e276db5943d632
Matt Westcott <m.westcott@gmail.com>
2023-03-10 21:52:17 +0000

    vim: adopt ChatGPT.nvim

--- vim/init.lua
+++ vim/init.lua
@@ -27,6 +27,53 @@ require('lazy').setup({
     event = "VimEnter",
   },
   {
+    'jackMort/ChatGPT.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require("chatgpt").setup({
+        welcome_message = "",
+        question_sign = "Q:",
+        answer_sign = "A:",
+        max_line_length = 120,
+        chat_input = {
+          prompt = " ",
+        },
+        openai_params = {
+          model = "gpt-3.5-turbo",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 300,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        openai_edit_params = {
+          model = "code-davinci-edit-001",
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        keymaps = {
+          close = { "<C-c>", "<Esc>" },
+          yank_last = "<C-y>",
+          scroll_up = "<C-u>",
+          scroll_down = "<C-d>",
+          toggle_settings = "<C-o>",
+          new_session = "<C-n>",
+          cycle_windows = "<Tab>",
+          -- In the Sessions pane:
+          select_session = "<Space>",
+          rename_session = "r",
+          delete_session = "d",
+        },
+      })
+    end,
+  },
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       'williamboman/mason.nvim',
@@ -111,6 +158,10 @@ vim.keymap.set('', '<C-b>', ':bn<CR>', { noremap = true })
 vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', { silent = true })
 vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', { silent = true })
 vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-dismiss)', { silent = true })
+
+-- ChatGPT
+vim.keymap.set('n', ',c', ':ChatGPT<CR>')
+vim.keymap.set({'n', 'v'}, ',e', ':<C-U>:ChatGPTEditWithInstructions<CR>')
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
