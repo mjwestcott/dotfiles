47e286e5322908618ac788678dd37c3a80a5d100
Matt Westcott <m.westcott@gmail.com>
2023-03-15 09:00:57 +0000

    vim: add trouble.nvim

--- vim/init.lua
+++ vim/init.lua
@@ -74,6 +74,10 @@ require('lazy').setup({
     end,
   },
   {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       'williamboman/mason.nvim',
@@ -176,6 +180,14 @@ vim.keymap.set('i', '<C-\\>', '<Plug>(copilot-dismiss)', { silent = true })
 -- ChatGPT
 vim.keymap.set('n', ',c', ':ChatGPT<CR>')
 vim.keymap.set({'n', 'v'}, ',e', ':<C-U>:ChatGPTEditWithInstructions<CR>')
+
+-- Trouble.nvim
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", {silent = true, noremap = true})
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", {silent = true, noremap = true})
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
