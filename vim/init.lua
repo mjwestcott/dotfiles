cbf802a607b695873983c26550643659c01d5b1a
Matt Westcott <m.westcott@gmail.com>
2023-03-15 09:01:19 +0000

    vim: add null-ls

--- vim/init.lua
+++ vim/init.lua
@@ -77,6 +77,7 @@ require('lazy').setup({
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
+  'jose-elias-alvarez/null-ls.nvim',
   {
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -363,6 +364,40 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+local null_ls = require 'null-ls'
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
+    sources = {
+        null_ls.builtins.formatting.trim_whitespace,
+        null_ls.builtins.completion.spell,
+        -- Python
+        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        -- Typescript
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.prettier,
+        -- Shell
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.formatting.shfmt,
+        -- Lua
+        null_ls.builtins.formatting.stylua,
+    },
+})
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
