e0b9a35c0323f8e498b097fe616c0f65f13b7bc8
Matt Westcott <m.westcott@gmail.com>
2023-03-24 13:11:35 +0000

    vim: remove mypy diagnostic

--- vim/init.lua
+++ vim/init.lua
@@ -409,7 +409,6 @@ null_ls.setup({
     null_ls.builtins.completion.spell,
     -- Python
     null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
     -- Typescript
